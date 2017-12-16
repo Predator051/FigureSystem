d8a8c854e7cca6307f7de800b3a876334713e573
Alexey Kukishev <aleksey.kukishev@gmail.com>
2017-12-16 21:37:52 +0200

    Start

---
+++ FigureSystem/FigureSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
